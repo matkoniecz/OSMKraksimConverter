8bf94953dbcf227f64205ef6ae3a6489eb863f21
DanOgiela <dan.ogiela@gmail.com>
2016-09-13 14:02:23 +0200

    converted part of internal structure to xml file (block 2)

--- converter_printer.py
+++ converter_printer.py
@@ -1,4 +1,42 @@
+# -*- coding: utf-8 -*-
+import codecs
+
 import lxml.etree as ET
+# import sys    # sys.setdefaultencoding is cancelled by site.py
+# reload(sys)    # to re-enable sys.setdefaultencoding()
+# sys.setdefaultencoding('utf-8')
+import re, htmlentitydefs
+
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text  # leave as is
+
+    return re.sub("&#?\w+;", fixup, text)
+
+def correct_xml_file(file_path):
+    f = codecs.open(file_path + ".xml", "r+", "utf-8")
+    text = unescape(f.read())
+    f.close()
+    f = codecs.open(file_path + ".xml", "w+", "utf-8")
+    f.write(text)
+    f.close()
 
 
 class ConverterPrinter:
@@ -8,15 +46,30 @@ class ConverterPrinter:
     @staticmethod
     def print_to_file(file_path, gateways, junctions, roads):
         road_net = ET.Element("roadnet")
-        nodes = ET.SubElement(road_net, "nodes")
 
+        # ------------------------ Blok nr 1 ----------------------------------
+        nodes_branch = ET.SubElement(road_net, "nodes")
         for gateway in gateways:
-            ET.SubElement(nodes, "gateway", id=str(gateway.id), x=str(gateway.x), y=str(gateway.y))
+            ET.SubElement(nodes_branch, "gateway", id=str(gateway.id), x=str(gateway.x), y=str(gateway.y))
 
         for junction in junctions:
-            ET.SubElement(nodes, "intersection", id=str(junction.id), x=str(junction.x), y=str(junction.y))
+            ET.SubElement(nodes_branch, "intersection", id=str(junction.id), x=str(junction.x), y=str(junction.y))
+
+        # ------------------------ Blok nr 2 ----------------------------------
+        roads_branch = ET.SubElement(road_net, "roads")
+        for road in roads:
+            road_branch = ET.SubElement(roads_branch, "road", id=str(road.id), street=road.street_name,
+                          attrib={'from':str(road.starting_point.id), 'to':str(road.ending_point.id)})
+            uplink_branch = ET.SubElement(road_branch, "uplink")
+            ET.SubElement(uplink_branch, "main", length=str(road.calculate_length()), numberOfLanes=str(road.lanes_number))
+            if not road.oneway:
+                downlink_branch = ET.SubElement(road_branch, "downlink")
+                ET.SubElement(downlink_branch, "main", length=str(road.calculate_length()), numberOfLanes=str(road.lanes_number))
 
         tree = ET.ElementTree(road_net)
         tree.write(file_path + ".xml", pretty_print=True)
 
+        correct_xml_file(file_path)
+
         pass
+

dce53d5f3f6565f09ecc83bf637053f36c35cb36
Mateusz Konieczny <matkoniecz@gmail.com>
2017-05-22 23:37:58 +0200

    add binary search minimal testcase generator

--- converter_runner.py
+++ converter_runner.py
@@ -19,6 +19,49 @@ def get_data(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast):
     result = ConverterNormalizer.simplify_loaded_data(result)
     return result, query
 
+def is_normalization_failed(result):
+    try:
+        ConverterNormalizer.validate_returned_data(result)
+    except ConverterNormalizer.ConversionFailed, error:
+        return True
+    return False
+
+def generate_minimal_test_case_if_errors_are_present(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast):
+    result, _ = get_data(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast)
+    latitudeSouth = float(latitudeSouth)
+    longitudeWest = float(longitudeWest)
+    latitudeNorth = float(latitudeNorth)
+    longitudeEast = float(longitudeEast)
+    if is_normalization_failed(result):
+        binary_search_for_problems(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast)
+
+def binary_search_for_problems(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast):
+    latitudeMiddle = (latitudeSouth + latitudeNorth) / 2
+    longitudeMiddle = (longitudeWest + longitudeEast) / 2
+
+    # bottom-left quadrant
+    result, _ = get_data(latitudeSouth, longitudeWest, latitudeMiddle, longitudeMiddle)
+    if is_normalization_failed(result):
+        return binary_search_for_problems(latitudeSouth, longitudeWest, latitudeMiddle, longitudeMiddle)
+
+    # upper-left quadrant
+    result, _ = get_data(latitudeMiddle, longitudeWest, latitudeNorth, longitudeMiddle)
+    if is_normalization_failed(result):
+        return binary_search_for_problems(latitudeMiddle, longitudeWest, latitudeNorth, longitudeMiddle)
+
+    # bottom-right quadrant
+    result, _ = get_data(latitudeSouth, longitudeMiddle, latitudeMiddle, longitudeEast)
+    if is_normalization_failed(result):
+        return binary_search_for_problems(latitudeSouth, longitudeMiddle, latitudeMiddle, longitudeEast)
+
+    # upper-right quadrant
+    result, _ = get_data(latitudeMiddle, longitudeMiddle, latitudeNorth, longitudeEast)
+    if is_normalization_failed(result):
+        return binary_search_for_problems(latitudeMiddle, longitudeMiddle, latitudeNorth, longitudeEast)
+
+    coords = str(latitudeSouth) + ' ' + str(longitudeWest) + ' ' + str(latitudeNorth) + ' ' + str(longitudeEast)
+    print 'following areas is smallest found by binary search: ' + coords
+
 if __name__ == "__main__":
     latitudeSouth, longitudeWest, latitudeNorth, longitudeEast = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
     result, query = get_data(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast)
@@ -26,6 +69,7 @@ if __name__ == "__main__":
     converter_reader.read_to_internal_structure(result)
     ConverterPrinter.print_to_file(sys.argv[5], converter_reader.gateways, converter_reader.junctions,
                                    converter_reader.ways)
+    generate_minimal_test_case_if_errors_are_present(latitudeSouth, longitudeWest, latitudeNorth, longitudeEast)
 
     # for way in result.ways:
     #     print("Name: %s" % way.tags.get("name", "n/a"))
